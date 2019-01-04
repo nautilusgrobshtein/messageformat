19c701f791157d11eb06b5582d2d85130ca47a68
! <>
2019-01-04 18:10:01 +0700

    Added lv

--- plurals.js
+++ plurals.js
@@ -47,6 +47,17 @@ module.exports = {
 		|| n == 800)) ? 'many' : 'other';
 		return (n == 1 && v0) ? 'one' : 'other';
 	},
+	lv: function(n, ord) {
+		var s = String(n).split('.'), f = s[1] || '', v = f.length,
+			t0 = Number(s[0]) == n, n10 = t0 && s[0].slice(- 1),
+			n100 = t0 && s[0].slice(- 2), f100 = f.slice(- 2), f10 = f.slice(- 1);
+		if (ord) return 'other';
+		return (t0 && n10 == 0 || (n100 >= 11 && n100 <= 19)
+			|| v == 2 && (f100 >= 11 && f100 <= 19)) ? 'zero'
+			: (n10 == 1 && n100 != 11 || v == 2 && f10 == 1 && f100 != 11
+				|| v != 2 && f10 == 1) ? 'one'
+				: 'other';
+	},
 	nl: _cp[3],
 	root: _cp[0],
 	ru: function(n, ord) {
